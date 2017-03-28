1e5e13a0df619e9ca72248b9cb1a25cc160b8a0a
huyou <hu_you@suixingpay.com>
2017-03-28 21:17:16 +0800

    注释

--- src/main/java/com/youhu/spring4_di/di/Main.java
+++ src/main/java/com/youhu/spring4_di/di/Main.java
@@ -9,9 +9,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class Main {
     public static void main (String [] args){
-
+        //1.获取context上下文(作为Spring容器)
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DiConfig.class);
+        //2.创建实例（注意并不是通过new的方式）
         UseDiService useDiService= context.getBean(UseDiService.class);
+
         System.out.println(useDiService.useDiFunction());
         context.close();
     }
